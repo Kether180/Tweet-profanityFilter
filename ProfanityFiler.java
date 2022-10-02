ac8ae829d821c6c69a10d56506fcda8258c0d71e
G.A <42187739+Kether180@users.noreply.github.com>
2022-10-03 00:15:19 +0200

    replacecharacters

--- ProfanityFiler.java
+++ ProfanityFiler.java
@@ -17,46 +17,46 @@ public class ProfanityFiler {
 
     tweetList.add(input.nextLine());
     // Iterator<String> input = tweetList.iterator();
-    while (input.hasNextLine()) { // Working :) , scanner input is having loads of strings but not replacing all characters like .,!? or FRIKinG or Fruit? and COUNT
-      // String item = input.nextLine();
+    // while (input.hasNextLine()) { // Working :) , scanner input is having loads of strings but not replacing all characters like .,!? or FRIKinG or Fruit? and COUNT
+    // String item = input.nextLine();
 
-      HashMap<String, String> swearWords = new HashMap<String, String>();
+    HashMap<String, String> swearWords = new HashMap<String, String>();
 
-      for (String string : splitTweet) {
-        String symbolString = "";
+    for (String string : splitTweet) {
+      String symbolString = "";
 
-        int index = 0;
+      int index = 0;
 
-        for (int i = 0; i < string.length(); i++) {
-          if (index == characterString.length) {
-            index = 0;
-          }
-          symbolString += characterString[index];
-          index++;
+      for (int i = 0; i < string.length(); i++) {
+        if (index == characterString.length) {
+          index = 0;
         }
-        swearWords.put(string.toLowerCase(), symbolString);
+        symbolString += characterString[index];
+        index++;
       }
-      // Double forloop and now it replaces your word with the new version.
-
-      // It doesn't work on fudgesicles. because it has a (.) "point" after it so you need to remove it first.
-
-      // I think I need to write another forloop for replacing the characteres that are at the end of the word fudgesicles like (.,!?)
-
-      for (String tweet1 : tweetList) {
-        String[] tweet1AsArray = tweet1.split(" ");
-        String newSting = "";
-        // String charac = ".,!?"; Tried to add it to this for loop in order to replace words with those characters at the end and it doesn't work
-        for (String string : tweet1AsArray) {
-          if (swearWords.containsKey(string)) { // || swearWords.containsKey(charac)
-            string = swearWords.get(string);
-          }
-          newSting += string + " ";
-        }
-        System.out.println(newSting);
-      }
-
-      input.close();
+      swearWords.put(string.toLowerCase(), symbolString);
     }
+    // Double forloop and now it replaces your word with the new version.
+
+    // It doesn't work on fudgesicles. because it has a (.) "point" after it so you need to remove it first.
+
+    // I think I need to write another forloop for replacing the characteres that are at the end of the word fudgesicles like (.,!?)
+
+    for (String tweet1 : tweetList) {
+      String[] tweet1AsArray = tweet1.split(" ");
+      String newSting = "";
+
+      // String charac = ".,!?"; Tried to add it to this for loop in order to replace words with those characters at the end and it doesn't work
+      for (String string : tweet1AsArray) {
+        if (swearWords.containsKey(string)) { // || swearWords.containsKey(charac)
+          string = swearWords.get(string);
+        }
+        newSting += string + " ";
+      }
+      System.out.println(newSting.replace(".", "$")); // substitute words with .,!? with *&#$ still finding out
+    }
+
+    input.close();
   }
 }
 // hate fudgesicles
