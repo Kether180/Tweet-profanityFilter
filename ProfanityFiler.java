c84b0c20c490767b7482c6628ea296f549e464c7
G.A <42187739+Kether180@users.noreply.github.com>
2022-10-02 16:01:30 +0200

    solving-while-loop-scanner

--- ProfanityFiler.java
+++ ProfanityFiler.java
@@ -22,40 +22,51 @@ public class ProfanityFiler {
     while (input.hasNextLine()) { // Working ;) , scanner input is having loads of strings but not replacing all swearwords and printing an extra line.
       String item = input.nextLine();
       if (item.equals("hate")) {/* still trying to solve it */
-        input.remove();
-      } else System.out.println(item);
-    }
-
-    HashMap<String, String> swearWords = new HashMap<String, String>();
-
-    for (String string : splitTweet) {
-      String symbolString = "";
-      int index = 0;
-      for (int i = 0; i < string.length(); i++) {
-        if (index == characterString.length) {
-          index = 0;
-        }
-        symbolString += characterString[index];
-        index++;
+        System.out.println("*&#$%");
+      } else {
+        System.out.println(item);
+      }
+      if (item.equals("fudgesicles")) {/* still trying to solve it */
+        System.out.println("*&#$%*&#$%*");
+      }
+      if (item.equals("FRICK")) {/* still trying to solve it */
+        System.out.println("*&#$%");
+      }
+      if (item.equals("fruit?")) {/* still trying to solve it */
+        System.out.println("*&#$%");
       }
 
-      swearWords.put(string.toLowerCase(), symbolString);
-    }
+      HashMap<String, String> swearWords = new HashMap<String, String>();
 
-    for (String tweet1 : tweetList) {
-      System.out.println(tweet1);
-    }
+      for (String string : splitTweet) {
+        String symbolString = "";
+        int index = 0;
+        for (int i = 0; i < string.length(); i++) {
+          if (index == characterString.length) {
+            index = 0;
+          }
+          symbolString += characterString[index];
+          index++;
+        }
 
-    System.out.print(
-      swearWords.get("hate".toLowerCase()) +
-      " " +
-      swearWords.get("fudgeSicles".toLowerCase())
-    );
-    input.close();
+        swearWords.put(string.toLowerCase(), symbolString);
+      }
+
+      for (String tweet1 : tweetList) {
+        System.out.println(tweet1);
+      }
+
+      System.out.print(
+        swearWords.get("hate".toLowerCase()) +
+        " " +
+        swearWords.get("fudgeSicles".toLowerCase())
+      );
+      input.close();
+    }
   }
 }
 // hate fudgesicles
 // I bleeping hate those fudgesicles.
-// I cannot ear another FRICKing  meatball
+// I cannot eat another FRICKing  meatball
 // I wonder how much fruit?  is actually a fruitcake?  fruit?
 // the COUNT
