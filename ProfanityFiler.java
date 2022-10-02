1e3ce43379aa0769d106efe30cecd96820db6706
G.A <42187739+Kether180@users.noreply.github.com>
2022-10-02 21:15:17 +0200

    replacing-characters

--- ProfanityFiler.java
+++ ProfanityFiler.java
@@ -9,73 +9,54 @@ public class ProfanityFiler {
 
     String tweet = input.nextLine();
 
-    String[] splitTweet = tweet.split(" "); // // split the string
+    String[] splitTweet = tweet.split(" ");
 
-    String[] characterString = { "*", "&", "#", "$", "%" }; // storage the characters
+    String[] characterString = { "*", "&", "#", "$", "%" };
 
-    // String [] badWords =
-
-    ArrayList<String> tweetList = new ArrayList<String>(); // arrayList to storage the tweet's inputs
+    ArrayList<String> tweetList = new ArrayList<String>();
 
     tweetList.add(input.nextLine());
-
     // Iterator<String> input = tweetList.iterator();
-    /* 
-        while (input.hasNextLine()) { // Working ;) , scanner input is having loads of strings but not replacing all swearwords and printing an extra line.
-        String item = input.nextLine();
-        // String itemoutput =input.nextLine();
-        if (item.equals("hate")) {/* still trying to solve it 
-        System.out.println("*&#$%");
-        } else {
-        System.out.println(item);
+    while (input.hasNextLine()) { // Working :) , scanner input is having loads of strings but not replacing all characters like .,!? or FRIKinG or Fruit? and COUNT
+      // String item = input.nextLine();
+
+      HashMap<String, String> swearWords = new HashMap<String, String>();
+
+      for (String string : splitTweet) {
+        String symbolString = "";
+
+        int index = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+          if (index == characterString.length) {
+            index = 0;
+          }
+          symbolString += characterString[index];
+          index++;
         }
-          if (item.equals("fudgesicles")) {/* still trying to solve it 
-          System.out.println("*&#$%*&#$%*");
-
-          }
-          if (item.equals("FRICK")) {/* still trying to solve it 
-          System.out.println("*&#$%");
-          }
-          if (item.equals("fruit?")) {/* still trying to solve it *
-          System.out.println("*&#$%");
-          }
-          */
-
-    HashMap<String, String> swearWords = new HashMap<String, String>();
-
-    for (String string : splitTweet) {
-      String symbolString = "";
-
-      int index = 0;
-
-      for (int i = 0; i < string.length(); i++) {
-        if (index == characterString.length) {
-          index = 0;
-        }
-        symbolString += characterString[index];
-        index++;
+        swearWords.put(string.toLowerCase(), symbolString);
       }
-      swearWords.put(string.toLowerCase(), symbolString);
-    }
-    // i made i double forloop and now it replaces your word with the new version.
+      // Double forloop and now it replaces your word with the new version.
 
-    //it does work on fudgesicles. because it has a . after it so you need to remove it first.
+      // It doesn't work on fudgesicles. because it has a (.) "point" after it so you need to remove it first.
 
-    //but this should get you going
+      // I think I need to write another forloop for replacing the characteres that are at the end of the word fudgesicles like (.,!?)
 
-    for (String tweet1 : tweetList) {
-      String[] tweet1AsArray = tweet1.split(" ");
-      String newSting = "";
-      for (String string : tweet1AsArray) {
-        if (swearWords.containsKey(string)) {
-          string = swearWords.get(string);
+      for (String tweet1 : tweetList) {
+        String[] tweet1AsArray = tweet1.split(" ");
+        String newSting = "";
+        // String charac = ".,!?"; Tried to add it to this for loop in order to replace words with those characters at the end and it doesn't work
+        for (String string : tweet1AsArray) {
+          if (swearWords.containsKey(string)) { // || swearWords.containsKey(charac)
+            string = swearWords.get(string);
+          }
+          newSting += string + " ";
         }
-        newSting += string + " ";
+        System.out.println(newSting);
       }
-      System.out.println(newSting);
-    }
 
-    input.close();
+      input.close();
+    }
   }
 }
 // hate fudgesicles
