0ee9a141069b37cdcce0c5fb89c93fbc068c3c4c
G.A <42187739+Kether180@users.noreply.github.com>
2022-10-06 21:50:19 +0200

    finalmente

--- ProfanityFilter.java
+++ ProfanityFilter.java
@@ -5,100 +5,86 @@ import java.util.Scanner;
 public class ProfanityFilter {
 
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    try (Scanner input = new Scanner(System.in)) {
+      String tweet = input.nextLine();
 
-    String tweet = input.nextLine();
+      String tweetToLowerCase = tweet.toLowerCase();
 
-    String tweetToLowerCase = tweet.toUpperCase();
+      String[] splitTweet = tweetToLowerCase.split(" ");
 
-    String[] splitTweet = tweetToLowerCase.split(" ");
+      String[] characterString = { "*", "&", "#", "$", "%" };
 
-    String[] characterString = { "*", "&", "#", "$", "%" };
+      String sPuntuacion;
 
-    ArrayList<String> tweetList = new ArrayList<String>();
+      String sResultado = "";
 
-    while (input.hasNextLine()) {
-      String tweetToList = input.nextLine();
-      String TweetToListToLowerCase = tweetToList; // took out lowercase
-      String tweetToListClean = TweetToListToLowerCase;
-      // .replaceAll(
-      //   "[,.!?]*",
-      //   ""
-      // );
-      tweetList.add(tweetToListClean);
-      if (!input.hasNextLine()) {
-        break;
-      }
-    }
+      ArrayList<String> tweetList = new ArrayList<String>();
 
-    HashMap<String, String> swearWords = new HashMap<String, String>();
-
-    for (String string : splitTweet) {
-      String symbolString = "";
-
-      int index = 0;
-      for (int i = 0; i < string.length(); i++) {
-        if (index == characterString.length) {
-          index = 0;
+      while (input.hasNextLine()) {
+        String tweetToList = input.nextLine();
+        String TweetToListToLowerCase = tweetToList;
+        String tweetToListClean = TweetToListToLowerCase;
+        tweetList.add(tweetToListClean);/* sentence to arraylist */
+        if (!input.hasNextLine()) {
+          break;
         }
-        symbolString += characterString[index];
-        index++;
       }
 
-      if (string.equals("FRUIT")) {
-        swearWords.put(string.toUpperCase(), symbolString + "?");
-      } else {
-        swearWords.put(string.toUpperCase(), symbolString);
-      }
-    }
+      HashMap<String, String> swearWords = new HashMap<String, String>();
 
-    for (String tweet1 : tweetList) {
-      int tweetSize = tweet1.length();
-      String stringdot;
+      for (String string : splitTweet) {
+        String symbolString = "";
 
-      stringdot = tweet1.substring(tweetSize - 1, tweetSize);
-
-      String[] tweet1AsArray = tweet1.split(" ");
-      String newSting = "";
-
-      for (String string : tweet1AsArray) {
-        String str;
-        str = string.toUpperCase();
-        if (
-          swearWords.containsKey(str.toUpperCase().replaceAll("[,.!?]*", ""))
-        ) {
-          string =
-            swearWords.get(string.toUpperCase().replaceAll("[,.!?]*", ""));
+        int index = 0;
+        for (int i = 0; i < string.length(); i++) {
+          if (index == characterString.length) {
+            index = 0;
+          }
+          symbolString += characterString[index];
+          index++;
         }
 
-        newSting += string + " ";
+        swearWords.put(string.toLowerCase(), symbolString);
       }
 
-      int sizestring = newSting.length();
+      for (String tweet1 : tweetList) {
+        String[] arrWordsFromSentence = tweet1.split(" ");
 
-      // newSting = newSting.substring(0, sizestring - 2);
+        for (String sword : arrWordsFromSentence) {
+          //System.out.println(sword);
 
-      if (newSting.equals("Fudgesicles")) {
-        newSting.substring(0, sizestring - 1);
-      } else {
-        newSting.substring(0, sizestring - 2);
+          sPuntuacion = "";
+          if (sword.contains(".")) {
+            //nIndexPoint = sword.index
+            sPuntuacion = ".";
+            sword = sword.replace(sPuntuacion, "");
+          }
+          if (sword.contains("?")) {
+            sPuntuacion = "?";
+            sword = sword.replace(sPuntuacion, "");
+          }
+
+          if (sword.contains("!")) {
+            sPuntuacion = "!";
+            sword = sword.replace(sPuntuacion, "");
+          }
+
+          if (sword.contains(",")) {
+            sPuntuacion = ",";
+            sword = sword.replace(sPuntuacion, "");
+          }
+          //System.out.println(sword);
+
+          if (swearWords.containsKey(sword.toLowerCase())) {
+            sword = swearWords.get(sword.toLowerCase());
+          }
+          //System.out.println(sword);
+
+          sResultado += sword + sPuntuacion + " ";
+        }
       }
-
-      System.out.println(newSting.replace(".", "") + stringdot);
+      sResultado = sResultado.substring(0, sResultado.length() - 1);
+      System.out.println(sResultado);
     }
-    // .replace(".", "").replace("?", "")
-    input.close();
   }
 }
-// Input 1
-// hate fudgesicles
-// I bleeping hate those fudgesicles.
-// Input 2
-// fricking
-// I cannot eat another FRICKing meatball!
-// Input 3
-// fruit
-// I wonder how much fruit? is actually in a fruitcake?
-// Input 4
-// the COUNT
-// Still I bleeping *&#$ those *&#$%*&#$%*.
